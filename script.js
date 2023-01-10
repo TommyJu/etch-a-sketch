0e4413733c286486bcdc1263960bce098bad0cac
Tommy Ju <tommyju25@gmail.com>
2023-01-09 18:00:46 -0800

    Add draw feature and some user controls

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const canvas = document.querySelector('#canvas');
 const body = document.querySelector('body');
 const slider = document.querySelector('#slider');
 const sliderValue = document.querySelector('.slider-value');
-
+const eraser = document.querySelector('#eraser');
+const drawBlack = document.querySelector('#draw-black');
+const colorPicker = document.querySelector('#color-picker');
+const previousColorDivs = document.querySelectorAll('#color-1, #color-2, #color-3');
 // Tracking mouse up and mouse down to a create a hold effect for drawing
 let mouseIsDown = false;
 
@@ -55,6 +58,7 @@ function draw(element, drawColor) {
     element.style.backgroundColor = drawColor;
 };
 
+// User controls --------------------------------------
 // Grid size slider
     
     // Helper function to clear all children of a DOM element
@@ -71,7 +75,42 @@ slider.oninput = () => {
     createCanvas(slider.value);
 }
 
-// Color selector
+eraser.addEventListener('click', () => {
+    drawColor = 'white';
+})
 
+drawBlack.addEventListener('click', () => {
+    drawColor = 'black';
+})
+
+// Color selector
+colorPicker.oninput = () => {
+    drawColor = colorPicker.value;
+}
+colorPicker.addEventListener('click', () => {
+    drawColor = colorPicker.value;
+    //prevents the same color from being added to the array twice
+    if (colorPicker.value != previousColors[2]) {
+        previousColors.shift();
+        previousColors.push(drawColor);
+        previousColorDivs.forEach((colorDiv, index) => {
+            colorDiv.style.backgroundColor = previousColors[index];
+        })
+    }
+})
+
+// Previous colors
+    // previous color stored as 'prevColor div' background color
+    // We can have a 3 element array that stores the 3 previous colors as a hex value
+    // on click from the color wheel, push the color value onto array and delete
+    // the first value. Then, overwrite the prevColor divs background colors
+    // according to the array,
+
+let previousColors = ['white', 'white', 'white'];
+previousColorDivs.forEach((colorDiv) => {
+    colorDiv.addEventListener('click', () => {
+        drawColor = colorDiv.style.backgroundColor;
+    })
+})
 
 createCanvas(canvasSize);
